3146e9d5752b9963efdcb7fcc973b15b861d4af7
Rana Dolui <ranadolui718@gmail.com>
2024-09-03 16:14:56 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-nj+!-sbc%akxkjn1q()q^1&q6wicw%+wd$69is_tck4h&)$isc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['text-tjgj.onrender.com', '127.0.0.1']
+# api/settings.py
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 
