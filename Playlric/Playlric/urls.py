e9e51afb17f9f884e3cab952a922556aa7c6c1b0
Rana Dolui <ranadolui718@gmail.com>
2024-10-05 15:56:47 +0530

    Update urls.py

--- Playlric/Playlric/urls.py
+++ Playlric/Playlric/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello, name='hello')
     path('videos/', include('song_info.urls')),
 ]
